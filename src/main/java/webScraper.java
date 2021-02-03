51f0748ed390898ec8859ce758d7aa277de6404f
deyizhang <deyizhang2003@gmail.com>
2021-02-03 16:21:09 -0500

    Commit

--- src/main/java/webScraper.java
+++ src/main/java/webScraper.java
@@ -7,23 +7,30 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class webScraper {
+    public static String bank;
+
     public static void main(String []args) throws IOException, InterruptedException {
         scraper();
     }
 
     public static void scraper() throws IOException, InterruptedException {
         System.out.println("What do you want to search for on Kijiji? (use dashs)");
+        //use dashs to ensure the search works, spaces dont work
         Scanner in2 = new Scanner(System.in);
         String choice = in2.next();
         String url = "https://www.kijiji.ca/b-gta-greater-toronto-area/" + choice + "/k0l1700272?rb=true&dc=true";
+        //url of kijiji, assumes your in the GTA
+
         Document doc = Jsoup.connect(url).get();
         Elements product = doc.getElementsByClass("title ");
-//
+
 
         ArrayList<String> productList = new ArrayList<String>();
 
         for (Element i:product) {
-            productList.add(i.text());
+            Document doc1 = Jsoup.parse((i.text()));
+            Elements link = doc1.select("a");
+            productList.add(link.text());
         }
         System.out.println("There are " + product.size() + " " + choice + "s available for purchase right now!");
         Thread.sleep(1000);
@@ -50,8 +57,8 @@ public class webScraper {
             }
         }
         for(int i = 0; i< product.size();i++){
-            System.out.println(product.get(i) + " \n Link: ");
-            Thread.sleep(200);
+            System.out.println("Product: " + product.get(i));
+            Thread.sleep(50);
         }
     }
 }
